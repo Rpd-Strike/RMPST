84b0c9f65d17b66c3e432130c5ae6104cbe043b4
Felix Puscasu <puscasu.felix1@gmail.com>
2023-06-28 12:08:19 +0100

    Test parralel with restriction

--- src/calculus/tests/parser.rs
+++ src/calculus/tests/parser.rs
@@ -3,6 +3,8 @@ use crate::calculus::syntax::AtomicAction;
 use super::super::syntax::Process;
 use super::super::parser::parse;
 
+// TODO: Add tests for "+"
+
 fn run_tests(tests: Vec<(&str, &Process)>) 
 {
     for (test, expected) in tests {
@@ -79,7 +81,33 @@ fn test_restriction_2()
     let newY = Process::New("y".to_string(), Box::new(resA));
 
     run_tests(vec![
-        ("\\y.\\x.(x<b>.0 | x[c].0).0", &newY),
+        ("\\y.\\x.(x<b>.0 | x[c].0)", &newY),
+    ])
+}
+
+#[test]
+fn test_restriction_par_prio()
+{
+    let XsendB = AtomicAction::Send("x".to_string(), "b".to_string());
+    let XsendB_p = Process::Prefix(XsendB, Box::new(Process::End));
+
+    let XrecvC = AtomicAction::Receive("x".to_string(), "c".to_string());
+    let XrecvC_p = Process::Prefix(XrecvC, Box::new(Process::End));
+    
+    
+    let abs = Process::New("y".to_string(), 
+        Box::new(Process::New("x".to_string(), 
+            Box::new(XsendB_p)
+        ))
+    );
+    
+    let res = Process::Par(
+        Box::new(abs),
+        Box::new(XrecvC_p),
+    );
+
+    run_tests(vec![
+        ("\\y.\\x.x<b>.0 | x[c].0", &res),
     ])
 }
 
